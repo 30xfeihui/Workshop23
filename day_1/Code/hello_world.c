424c06d8059255f3f2b35f07d56cbe40bef37084
Labib Al Faisal <79709412+30xfeihui@users.noreply.github.com>
2023-03-06 11:08:04 +0600

    Add files via upload

---
+++ day_1/Code/hello_world.c
@@ -0,0 +1,19 @@
+// This is a Comment.. This line will be ignored by compiler
+
+// include header file for input and outputs
+// printf() function are decleared in stdio.h header
+#include<stdio.h>
+
+// main functoin starts from here
+int main(){
+	
+	// printf() function is used for output
+	// \n means printing new line
+	// don't forget to add ; in the end
+	printf("Hello World\n");
+	
+
+	// return 0 means here the program ends
+	return 0;
+
+}
